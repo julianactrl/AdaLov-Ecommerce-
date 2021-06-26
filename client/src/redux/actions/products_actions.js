266ea46b0a7d3a9c3264dd42fa43fe1bd4c2c58d
Juliana Gonzalez <juliana.mg3@gmail.com>
2021-06-25 21:39:44 -0300

    Initial commit

---
+++ client/src/redux/actions/products_actions.js
@@ -0,0 +1,194 @@
+import * as api from '../api/index.js';
+import * as constants from "../constants";
+
+
+//const { REACT_APP_API } = 'https://e-commerce-g6-back.herokuapp.com/'; // En local comentar esta linea
+const { REACT_APP_API } = process.env; // En deploy comentar esta linea
+//=============================================================================//
+export const getAllProducts = (page,custom) => async (dispatch) => {
+  dispatch({
+    type: GET_ALL_PRODUCTS,
+  });
+  return await api.getAllProducts(page,custom)
+    .then((res) => {
+      dispatch({
+        type: constants.GET_PRODUCTS_SUCCESS,
+        payload: res.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: constants.GET_PRODUCTS_ERROR,
+        payload: err.response,
+      });
+    });
+};
+//=============================================================================//
+export const searchProducts = (name) => async(dispatch) => {
+    return await api.searchProducts(name)
+      .then((res) => {
+        dispatch({ 
+          type: constants.SEARCH_PRODUCTS, 
+          payload: res.data 
+        });
+    })
+
+    .catch((error) => console.log(error));
+}
+//=============================================================================//
+export const detailProduct = (id) => async(dispatch)=> {
+    return await api.detailProduct(id)
+      .then((res) => {
+        dispatch({ 
+          type: constants.DETAIL_PRODUCT, 
+          payload: res.data 
+        });
+      })
+      .catch((error) => {
+        dispatch({
+          type: constants.GET_PRODUCTS_ERROR,
+          payload: error.payload
+      })
+  });
+}
+//=============================================================================//
+export const addProducts = (body) => async (dispatch) => {
+  dispatch({
+    type: constants.ADD_PRODUCT,    
+  });
+  return await api.addProducts(body)
+  .then((p) => {
+    dispatch({
+      type: constants.ADD_PRODUCT_SUCCESS,
+      payload: p.data,
+    });
+  })
+  .catch((err) => {
+    dispatch({
+      type: constants.ADD_PRODUCT_ERROR,
+      payload: err.response,
+    });
+  });
+};
+//=============================================================================//
+export const deleteProduct = (payload) => 
+  async(dispatch) => {
+    return await api.deleteProduct(payload)
+      .then(() => {
+      dispatch({
+        type: constants.DELETE_PRODUCT,
+        payload
+      })
+    })
+  .catch((error) => console.log(error))
+}
+//=============================================================================//
+export const editProduct = (payload) => async(dispatch) => {
+    return await api.editProduct(payload)
+      .then((product) => {
+        dispatch({
+          type: constants.EDIT_PRODUCT,
+          payload: product.data
+        })
+    })
+    .catch((error) => console.log(error))
+}
+//=============================================================================//
+export const stockUpdated = (id,quantity) => async(dispatch) => {
+  return await api.editStock(id,quantity)
+    .then((stock) => {
+      dispatch({
+        type: constants.EDIT_STOCK,
+        payload: stock.data
+      })
+  })
+  .catch((error) => console.log(error))
+}
+//=============================================================================//
+export const deleteProductStock = (id) => async(dispatch) => {
+  return await api.deleteStock(id)
+  .then((stock) => {
+      dispatch({
+        type: constants.DELETE_STOCK,
+        payload: stock.data
+      })
+  })
+  .catch((error) => console.log(error))
+}
+//=============================================================================//
+export const addProductDiscount = (productId, percentage) => async(dispatch) => {
+  dispatch({
+    type: constants.ADD_PRODUCT_DISCOUNT
+  })
+  return await api.addProductDiscount(productId, percentage)
+  .then((product) => {
+    dispatch({
+      type: constants.ADD_PRODUCT_DISCOUNT_SUCCESS,
+      payload: product.data
+    })
+  })
+  .catch((error) => {
+    dispatch({
+      type: constants.ADD_PRODUCT_DISCOUNT_ERROR,
+      payload: error.response?.data
+    })
+  }) 
+}
+//=============================================================================//
+export const removeProductDiscount = (productId) => async(dispatch) => {
+  dispatch({
+    type: constants.REMOVE_PRODUCT_DISCOUNT
+  })
+  return await api.removeProductDiscount(productId)
+  .then((product) => {
+    dispatch({
+      type: constants.REMOVE_PRODUCT_DISCOUNT_SUCCESS,
+      payload: product.data
+    })
+  })
+  .catch((error) => {
+    dispatch({
+      type: constants.REMOVE_PRODUCT_DISCOUNT_ERROR,
+      payload: error.response?.data
+    })
+  })
+}
+//=============================================================================//
+export const customProductApproval = (productId) => async(dispatch) => {
+  dispatch({
+    type: constants.APPROVE_CUSTOM_PRODUCT
+  })
+  return await api.customProductApproval(productId)
+  .then((product) => {
+    dispatch({
+      type: constants.APPROVE_CUSTOM_PRODUCT_SUCCESS,
+      payload: product.data
+    })
+  })
+  .catch((error) => {
+    dispatch({
+      type: constants.APPROVE_CUSTOM_PRODUCT_ERROR,
+      payload: error.response?.data
+    })
+  })
+}
+//=============================================================================//
+export const customProductDisapproval = (productId) => async(dispatch) => {
+  dispatch({
+    type: constants.DISAPPROVE_CUSTOM_PRODUCT
+  })
+  return await api.customProductDisapproval(productId)
+  .then((product) => {
+    dispatch({
+      type: constants.DISAPPROVE_CUSTOM_PRODUCT_SUCCESS,
+      payload: product.data
+    })
+  })
+  .catch((error) => {
+    dispatch({
+      type: constants.DISAPPROVE_CUSTOM_PRODUCT_ERROR,
+      payload: error.response?.data
+    })
+  })
+}
+//=============================================================================//
---
+++ client/src/redux/reducer/products_reducer.js
@@ -0,0 +1,82 @@
+import { stockUpdated } from "../actions/products_actions";
+import * as constants from "../constants";
+
+const initialState = {
+  allProducts: [],
+  addProduct: null,
+  isLoading: false,
+  error: null
+};
+
+const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case constants.GET_ALL_PRODUCTS:
+      return { ...state, allProducts: [], isLoading: true, error: null };
+    case constants.GET_PRODUCTS_SUCCESS:
+      return { ...state, allProducts: action.payload, isLoading: false, error: null };
+    case constants.GET_PRODUCTS_ERROR:
+      return { ...state, allProducts: [], isLoading: false, error: true };
+    case constants.ADD_PRODUCT:
+      return { ...state, addProduct: null, isLoading: true, error: null };
+    case constants.ADD_PRODUCT_SUCCESS:
+      return { ...state, addProduct: action.payload, isLoading: false, error: false };
+    case constants.ADD_PRODUCT_ERROR:
+      return { ...state, addProduct: null, isLoading: false, error: true }
+    case constants.EDIT_PRODUCT:
+      return {
+        ...state,
+        allProducts: state.allProducts.products.map((prod) => {
+          if (action.payload.id === prod.id) {
+            prod = {
+              ...prod,
+              ...action.payload,
+            }
+          }
+          return prod;
+        })
+      }
+    case constants.EDIT_STOCK:
+      return { ...state,
+      allProducts: action.payload}
+    case constants.DELETE_STOCK:
+      return{...state, allProducts:action.payload}
+    case constants.DELETE_PRODUCT:
+      return {...state, allProducts: state.allProducts.products.filter((prod) => prod.id !== action.payload)}
+    case constants.DETAIL_PRODUCT:
+      return { ...state, allProducts: action.payload };
+    case constants.SEARCH_PRODUCTS:
+      return { ...state, allProducts: action.payload };
+    case constants.FILTER_BY_NAME:
+      return { ...state, allProducts: action.payload }
+    case constants.FILTER_BY_PRICE:
+      return { ...state, allProducts: action.payload }
+    case constants.FILTER_BY_CATEGORY:
+      return { ...state, allProducts: action.payload }
+    case constants.FILTER_BY_BRAND:
+      return { ...state, allProducts: action.payload }
+    case constants.FILTER_BY_SIZE:
+      return { ...state, allProducts: action.payload }
+    case constants.ADD_PRODUCT_DISCOUNT:
+      return { ...state, allProducts: null, isLoading: true, error: null }
+    case constants.ADD_PRODUCT_DISCOUNT_SUCCESS:
+      return { ...state, allProducts: action.payload, isLoading: false, error: null }
+    case constants.ADD_PRODUCT_DISCOUNT_ERROR:
+      return { ...state, allProducts: null, isLoading: false, error: action.payload }
+    case constants.APPROVE_CUSTOM_PRODUCT:
+      return { ...state, allProducts: null, isLoading: true, error: null } 
+    case constants.APPROVE_CUSTOM_PRODUCT_SUCCESS:
+      return { ...state, allProducts: action.payload, isLoading: false, error: null }
+    case constants.APPROVE_CUSTOM_PRODUCT_ERROR:
+      return { ...state, allProducts: null, isLoading: false, error: action.payload }
+    case constants.DISAPPROVE_CUSTOM_PRODUCT:
+      return { ...state, allProducts: null, isLoading: true, error: null }
+    case constants.DISAPPROVE_CUSTOM_PRODUCT_SUCCESS:
+      return { ...state, allProducts: action.payload, isLoading: false, error: null }
+    case constants.DISAPPROVE_CUSTOM_PRODUCT_ERROR:
+      return { ...state, allProducts: null, isLoading: false, error: action.payload }
+    default:
+      return state;
+  }
+};
+
+export default productsReducer;
